139e5e2cc1a1b8d2a3423ebde00e5715f1087192
janakaud <janakaud@gmail.com>
2019-11-07 13:11:14 +0530

    added DynamoDB

--- animal-reserve-janaka/rekognizer.js
+++ animal-reserve-janaka/rekognizer.js
@@ -1,7 +1,8 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
@@ -14,9 +15,19 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+            let label = data.Labels[0].Name;
+            ddb.put({
+                TableName: 'slp-animals-janaka',
+                Item: { 'name': s3.object.key, 'label': label }
+            }).promise()
+                .then((data) => {
+                    callback(null, {});
+                })
+                .catch((err) => {
+                    callback(err);
+                });
+        })
+        .catch(err => callback(err));
 }
